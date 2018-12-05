86ba775d5bff2cfa7acc6d40d1052dd5e9fcdb0f
yurah370 <45288717+yurah370@users.noreply.github.com>
2018-12-06 04:16:02 +0900

    change address suffix

--- cashaddr_test.go
+++ cashaddr_test.go
@@ -9,7 +9,9 @@ import (
 	"github.com/btcsuite/btcd/chaincfg"
 )
 
-func TestFindwhcAddress(t *testing.T) {
+func TestFindwsvAddress(t *testing.T) {
+	const SUFFIX = "wsv"
+
 	for v := 0; v < 10000000; v++ {
 		x := fmt.Sprintf("%02x", v)
 		len1 := len(x)
@@ -29,16 +31,16 @@ func TestFindwhcAddress(t *testing.T) {
 			return
 		}
 
-		if strings.HasSuffix(address.String(), "whc") {
+		if strings.HasSuffix(address.String(), SUFFIX) {
 			addr1 := fmt.Sprintf("%v", address)
-			if addr1[len(addr1)-4] >= '0' && addr1[len(addr1)-4] <= '9' && strings.HasSuffix(addr1, "whc") {
+			if addr1[len(addr1)-4] >= '0' && addr1[len(addr1)-4] <= '9' && strings.HasSuffix(addr1, SUFFIX) {
 				addr2 := fmt.Sprintf("%v", addr1)
-				if strings.HasSuffix(addr2, "8whc") {
-					fmt.Println("===========find burn address last 4 8whc=========")
+				if strings.HasSuffix(addr2, "8"+SUFFIX) {
+					fmt.Println("===========find burn address last 4 8" + SUFFIX + "=========")
 					addr3 := fmt.Sprintf("%v", addr2)
 					fmt.Printf("the public hash value is:%v\nthe burn addr value is:%v\n", result, addr2)
-					if addr3[len(addr3)-5] >= '0' && addr3[len(addr3)-5] <= '9' && strings.HasSuffix(addr3, "whc") {
-						fmt.Println("===========find burn address last 5 [0-9]8whc=========")
+					if addr3[len(addr3)-5] >= '0' && addr3[len(addr3)-5] <= '9' && strings.HasSuffix(addr3, SUFFIX) {
+						fmt.Println("===========find burn address last 5 [0-9]8" + SUFFIX + "=========")
 						fmt.Printf("the public hash value is:%v\nthe burn addr value is:%v\n", result, addr3)
 					}
 				}
